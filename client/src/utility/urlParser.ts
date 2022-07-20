0f4f3344f82c73262e50b54f579d46a0a4576362
Edward Dobson <edw@rddobson.com>
2022-07-20 23:29:01 +0100

    Allow for URL variable injection

--- client/src/utility/urlParser.ts
+++ client/src/utility/urlParser.ts
@@ -8,6 +8,8 @@ export const urlParser = (url: string): string[] => {
     url = `http://${url}`;
   }
 
+  url = performReplacements(url);
+
   // Create simplified url to display as text
   let displayUrl: string;
   if (isSteamUrl(url)) {
@@ -20,4 +22,11 @@ export const urlParser = (url: string): string[] => {
   } else displayUrl = url;
 
   return [displayUrl, url];
+};
+
+const performReplacements = (url: string): string => {
+  return url
+    .replace('$host', location.host)
+    .replace('$hostname', location.hostname)
+    .replace('$port', location.port);
 };
